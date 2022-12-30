835dec69f48648f245e3a0ceb715a0ba4ae27cd2
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-12-30 22:33:02 +0600

    Update App.java

--- src/App.java
+++ src/App.java
@@ -10,11 +10,9 @@ public class App {
         int a;
         System.out.println("Wecome to The Data Structure project");
         System.out.println("Name of Developer:Mahady Hasan Fahim");
-        Scanner sc=new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
 
-
-        while(true){
+        while (true) {
             System.out.println("Enter The your chooese:\n");
             System.out.println("Prees 1 for Sorting\n");
             System.out.println("Press 2 for Searching\n");
@@ -22,43 +20,46 @@ public class App {
             System.out.println("Press 4 for Queue\n");
             System.out.println("Press 5 for LinkedList\n");
             System.out.println("Press 6 for Tree\n");
-           // System.out.println("Press 4 for Queue\n");
 
-               a=sc.nextInt();
-            
-            switch(a)
-            {
-                    case 1:
-                                  
-                                    break;
-                    case 2:
-                                
-                                    break;
-                    case 3: 
-                                    
-                                    break;
-                    case 4:
-               
-                                    break;
-                    case 5:
-               
-                                    break;
-                    case 6:
-               
-                                    break;
-                    case 7:
-               
-                                    break;
-                         
-                    default:
-                                  
-                                 
-                                    break;
+            a = sc.nextInt();
+
+            switch (a) {
+                case 1:
+
+                    Sorting ObjectSort = new Sorting();
+                    ObjectSort.sort();
+                    break;
+                case 2:
+                    Search Objectsearch = new Search();
+                    Objectsearch.search();
+                    break;
+                case 3:
+
+                    Stack objectctStack = new Stack();
+                    objectctStack.stack();
+
+                    break;
+                case 4:
+                    Queue objQueue = new Queue();
+                    objQueue.queue();
+                    break;
+                case 5:
+                    LinkedList obLinkedList = new LinkedList();
+                    obLinkedList.linkedlist();
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+
+                default:
+
+                    break;
             }
             sc.close();
 
-
-
-    }
+        }
     }
 }
