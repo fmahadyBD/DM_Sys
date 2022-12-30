9175d796da08bd9d4c6d204eb128c568f1ac87aa
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-12-30 22:35:05 +0600

    Update Search.java

--- src/Search.java
+++ src/Search.java
@@ -1,3 +1,69 @@
+import java.util.Scanner;
+
+
 public class Search {
+
+
+    public void  search(){
+
+        int a;
+        System.out.println("Wecome to Sorting System");
+
+        Scanner sc=new Scanner(System.in);
+
+        while(true){
+            System.out.println("Enter The your chooese:\n");
+            System.out.println("Prees 1 for Liniar\n");
+            System.out.println("Press 2 for Bainarary\n");
+
+            System.out.println("Press 3 for Return to Home\n");
+         
+               a=sc.nextInt();
+            
+            switch(a)
+            {
+                    case 1:
+                   Liniar();
+                   
+                                    break;
+                    case 2:
+                    Bainarary();
+                                    break;
+                    case 3: 
+
+                   
+                                    
+                                    break;
+   
+
+                         
+                    default:
+                                  
+                                 
+                                    break;
+            }
+            sc.close();
+
+
+
+    }
     
+
+    }
+
+
+    //____________
+
+    public int Liniar(){
+
+        return 0;
+
+    }
+    public int Bainarary(){
+
+        return 0;
+
+    }
+
+
 }
