c52ab6eb9fb92abd65edd9d293b5bd0d662612f9
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-12-31 21:26:39 +0600

    Update Stack.java

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,24 @@
-public class Stack {
-    public void stack(){
-        
+
+import java.util.Stack;
+public class Stacks {
+    public  void stack() {
+        Stack<Integer> stack = new Stack<>();
+
+        // push elements onto the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        // display the stack
+        System.out.println("Stack: " + stack);
+
+        // pop an element from the stack
+        int element = stack.pop();
+        System.out.println("Popped element: " + element);
+
+        // display the stack
+        System.out.println("Stack: " + stack);
     }
 }
