34ddaa7cbb5a1a480e74b63364ec73ff1bdce2d6
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-01 22:20:39 +0600

    Update Stacks.java

--- src/Stacks.java
+++ src/Stacks.java
@@ -1,15 +1,15 @@
-
 import java.util.Scanner;
 import java.util.Stack;
+
 public class Stacks {
     /**
      * @return
      * @throws Exception
      */
-    public  int stack() throws Exception {
-        int a,u;
+    public int stack() throws Exception {
+        int a, u;
         Stack<Integer> stack = new Stack<>();
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Value you want to push");
 
         while (true) {
@@ -23,34 +23,25 @@ public class Stacks {
 
             switch (a) {
                 case 1:
-                System.out.println("Push a value:\n");
-                a = sc.nextInt();
-                stack.push(2);
+                    System.out.println("Push a value:\n");
+                    a = sc.nextInt();
+                    stack.push(2);
                     break;
                 case 2:
-                int element = stack.pop();
-                System.out.println("Popped element: " + element);
+                    int element = stack.pop();
+                    System.out.println("Popped element: " + element);
                     break;
                 case 3:
-
-                System.out.println("Stack: " + stack);
-
+                    System.out.println("Stack: " + stack);
                     break;
                 case 4:
-                App obj=new App();
-                App.main(new String[]{});
- 
-
+                    App obj = new App();
+                    App.main(new String[] {});
                 default:
-
                     break;
             }
             sc.close();
         }
-      
-      
-        
-   
 
     }
 }
