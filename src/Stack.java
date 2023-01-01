dc683efee5c9bdde0df10f30d8e31820e101fea2
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-01 11:03:14 +0600

    Update Stack.java

--- src/Stack.java
+++ src/Stack.java
@@ -1,24 +1,54 @@
 
+import java.util.Scanner;
 import java.util.Stack;
 public class Stacks {
-    public  void stack() {
+    /**
+     * @return
+     */
+    public  int stack() {
+        int a,u;
         Stack<Integer> stack = new Stack<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Value you want to push");
 
-        // push elements onto the stack
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
+        while (true) {
+            System.out.println("Enter The your chooese:\n");
+            System.out.println("Prees 1 for Push\n");
+            System.out.println("Press 2 for Pop\n");
+            System.out.println("Press 3 for Disply\n");
+            System.out.println("Press 4 for Return Home\n");
 
-        // display the stack
-        System.out.println("Stack: " + stack);
+            a = sc.nextInt();
 
-        // pop an element from the stack
-        int element = stack.pop();
-        System.out.println("Popped element: " + element);
+            switch (a) {
+                case 1:
+                System.out.println("Push a value:\n");
+                a = sc.nextInt();
+                stack.push(2);
+                    break;
+                case 2:
+                int element = stack.pop();
+                System.out.println("Popped element: " + element);
+                    break;
+                case 3:
 
-        // display the stack
-        System.out.println("Stack: " + stack);
+                System.out.println("Stack: " + stack);
+
+                    break;
+                case 4:
+                return 0;
+ 
+
+                default:
+
+                    break;
+            }
+            sc.close();
+        }
+      
+      
+        
+   
+
     }
 }
