2ee7904522ca5c630a39d43f11701d0baf6ea444
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-01 11:51:34 +0600

    Add files via upload

---
+++ src/hmap.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+public class hmap {
+    void hash() throws Exception{
+        HashMap <String, Integer> em=new HashMap<>();
+        em.put("Mahady",1);
+        em.put("Fahim",2);
+        em.put("Hasan",3);
+        em.put("Sad",4);
+        em.put("Dark",5);
+        em.put("Light",6);
+        // it is way to added the value..
+        System.out.println(em);
+
+        System.out.println(em.get("Mahady"));//  it will show id
+        System.out.println(em.containsKey("Hasan"));// contain korele true
+        em.putIfAbsent("Fahim", 6);
+        System.out.println(em);
+        for(Map.Entry<String,Integer> e:em.entrySet()){
+
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+
+        }
+        System.out.println("Set:");
+
+        Set<String>keys=em.keySet();
+
+        for(String key:keys){
+            System.out.println(keys+ " "+em.get(keys));
+        }
+
+        App obj=new App();
+        App.main(new String[]{});
+
+
+    }
+}
