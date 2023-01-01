3b9e7b6e7faa4db18801f0bbbe8d172fb40ccd22
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-01 22:38:36 +0600

    Update Sorting.java

--- src/Sorting.java
+++ src/Sorting.java
@@ -5,60 +5,53 @@ public class Sorting {
     public int sort() throws Exception {
         int size;
         int b;
+
         System.out.println("Wecome to Sorting System");
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the array size: ");
         size = sc.nextInt();
-        int a[] = new int[size];
 
+        int a[] = new int[size];
         System.out.println("Enter the array Element:");
+
         for (int i = 0; i < size; i++) {
             a[i] = sc.nextInt();
         }
 
+        System.out.println("Enter The your chooese that you want to oparation:\n");
+        System.out.println("Prees 1 for Buubble\n");
+        System.out.println("Press 2 for count\n");
+        System.out.println("Press 3 for Selection\n");
+        System.out.println("Press 4 for Insertion\n");
+        System.out.println("Press othres for Return to Home\n");
 
-            System.out.println("Enter The your chooese that you want to oparation:\n");
-            System.out.println("Prees 1 for Buubble\n");
-            System.out.println("Press 2 for count\n");
-            System.out.println("Press 3 for Selection\n");
-            System.out.println("Press 4 for Insertion\n");
-            System.out.println("Press othres for Return to Home\n");
+        b = sc.nextInt();
 
-            b = sc.nextInt();
+        switch (b) {
+            case 1:
+                bubleSort(a);
+                break;
+            case 2:
+                count(a);
+                break;
+            case 3:
+                selection(a);
+                break;
+            case 4:
+                Insersiton(a);
+                break;
+            default:
 
-            switch (b) {
-                case 1:
-                    bubleSort(a);
-
-                    break;
-                case 2:
-                    count(a);
-                    break;
-                case 3:
-
-                    selection(a);
-
-                    break;
-                case 4:
-                    Insersiton(a);
-                    break;
-
-
-                default:
-                
-                App obj=new App();
-                App.main(new String[]{});
-                    break;
-            }
-
-            sc.close();
-            return 0;
-
+                App obj = new App();
+                App.main(new String[] {});
+                break;
         }
 
-    
+        sc.close();
+        return 0;
 
-    // ____________
+    }
 
     public int bubleSort(int[] numbers) throws InterruptedException {
 
@@ -74,13 +67,11 @@ public class Sorting {
             }
         }
 
-        // print the sorted array
         System.out.println("The result of BubbleSort : ");
         for (int i : numbers) {
             System.out.print(i + " ");
         }
         System.out.println();
-
         System.out.println("Your Array is now Sorted.. Thanks\n");
         return 0;
 
@@ -89,7 +80,6 @@ public class Sorting {
     public int count(int[] a) throws InterruptedException {
         int max = 0;
         System.out.println("___________________Welcome to CounSort Method.________________________");
-
 
         for (int i = 0; i <= a.length - 1; i++) {
             if (max < a[i]) {
@@ -128,28 +118,28 @@ public class Sorting {
         }
         System.out.println("Your Array is now Sorted.. Thanks\n");
 
-
         return 0;
 
     }
 
     public int selection(int[] numbers) throws InterruptedException {
+
         System.out.println("___________________Welcome to SelectionSort Method.________________________");
+
         for (int i = 0; i < numbers.length - 1; i++) {
-            // find the minimum element in the unsorted part of the array
+
             int minIndex = i;
             for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[j] < numbers[minIndex]) {
                     minIndex = j;
                 }
             }
-            // swap the minimum element with the first element of the unsorted part
+
             int temp = numbers[minIndex];
             numbers[minIndex] = numbers[i];
             numbers[i] = temp;
         }
 
-        // print the sorted array
         System.out.println("The result of Selection sort: ");
         for (int i : numbers) {
             System.out.print(i + " ");
@@ -165,7 +155,7 @@ public class Sorting {
         System.out.println("___________________Welcome to InsersitonSort Method.________________________");
 
         for (int i = 1; i < numbers.length; i++) {
-            // insert numbers[i] into the sorted part of the array
+
             int key = numbers[i];
             int j = i - 1;
             while (j >= 0 && numbers[j] > key) {
@@ -175,7 +165,6 @@ public class Sorting {
             numbers[j + 1] = key;
         }
 
-        // print the sorted array
         System.out.println("The result of Insertion sort: ");
         for (int i : numbers) {
             System.out.print(i + " ");
